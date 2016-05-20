427b2a70d7cadabc6628fb55a57ff47bd6519a8c
sukso96100 <sukso96100@gmail.com>
2016-05-20 15:29:47 +0900

    updated script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting Conlogger Bot Server"
-export SLACK_API_TOKEN = xoxb-44195058997-2FCNpaLL8kLi1eRpSzKd7vom;
-export GOOGLE_CLIENT_SECRET_FILE = client_secret_854196446304-m7au7k6i9gjko9cjt8m8pe09ck36f4lv.apps.googleusercontent.com.json;
-export GOOGLD_DRIVE_TARGET_FOLDER_ID = 0B5llRDmwiwWlLTNod01MdU5VSHc;
+export SLACK_API_TOKEN='xoxb-44195058997-2FCNpaLL8kLi1eRpSzKd7vom';
+export GOOGLE_CLIENT_SECRET_FILE='client_secret_854196446304-m7au7k6i9gjko9cjt8m8pe09ck36f4lv.apps.googleusercontent.com.json';
+export GOOGLD_DRIVE_TARGET_FOLDER_ID='0B5llRDmwiwWlLTNod01MdU5VSHc';
 node index.js
